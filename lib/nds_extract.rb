eb4794a764d9a239be0acec7cde73a085772369b
comical-sandbox-7108 <merewelch@gmail.com>
2019-11-06 13:32:14 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -18,8 +18,8 @@ def directors_totals(nds)
     end
 
     dir_hash = {directors_database[row_index][:name] => dir_gross}
+    puts dir_hash
     row_index += 1
-    puts dir_gross  
   end
   return dir_hash
   # Remember, it's always OK to pretty print what you get *in* to make sure
